e87af414d8823b84725319bf7758e8a441c32ead
Oleksiy Litvinov <leetwinoff92@gmail.com>
2024-07-22 13:04:59 +0200

    feat: add initial TodoApp implementation
    Implemented the initial version of TodoApp using React. This includes:
    - Basic app structure
    - Initial components
    - Initial state management
    - Basic styles
    
    This commit sets up the foundation for further development and enhancements.

---
+++ todoapp/src/App.jsx
@@ -0,0 +1,17 @@
+
+import ToDo from "./components/ToDo"
+import Header from "./components/Header"
+import "./App.css"
+
+function App() {
+  return (
+    <div className="App"></div>,
+    <div>
+      <Header />
+      <ToDo />
+      
+    </div>
+  )
+}
+
+export default App
---
+++ todoapp/src/components/Header.jsx
@@ -0,0 +1,7 @@
+import styles from "./header.module.css"
+
+export default function Header() {
+  return <div>
+    <h3 className={styles.header}>My todo list</h3>
+  </div>
+}
